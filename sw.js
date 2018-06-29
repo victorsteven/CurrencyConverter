dbf745e7faf7024f4e5684526571850ed4f7d07b
victorsteven <chikodi543@gmail.com>
2018-06-29 16:26:31 +0100

    registered a service worker in order to use cache to run the app

---
+++ sw.js
@@ -0,0 +1,37 @@
+
+var staticCacheName = 'currency-static-v2';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll([
+                './.nojekyll',
+                './index.html',
+                '/js/currency.js'  
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName.startsWith('currency-') && !staticCacheName.includes(cacheName);
+                }).map(function(cacheName){
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+//Use cache to run app
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
